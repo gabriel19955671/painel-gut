efe928622c784a5895254c2c1e363ac718600a7d
gabriel19955671 <gabriel.vilela@potencializeresultados.com>
2025-05-01 18:13:38 -0300

    Update Painel_Gut_Interativo.py

--- Painel_Gut_Interativo.py
+++ Painel_Gut_Interativo.py
@@ -249,7 +249,7 @@ with aba4:
             if os.path.exists("logo_PR_FIXA.png"):
                 pdf.image("logo_PR_FIXA.png", x=10, y=8, w=50)
             pdf.set_font("Arial", "B", 16)
-            pdf.set_y(20)
+            pdf.set_y(30)
             pdf.cell(0, 10, "Diagnóstico 360º - Potencialize Resultados", ln=True, align="C")
             pdf.set_font("Arial", "", 10)
             pdf.cell(0, 10, f"Cliente: {nome_cliente} | Data: {data_diagnostico.strftime('%d/%m/%Y')}", ln=True, align="C")
