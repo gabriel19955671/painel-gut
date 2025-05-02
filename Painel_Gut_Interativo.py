d0b2a70314fb354ae374b25525f823471667671c
gabriel19955671 <gabriel.vilela@potencializeresultados.com>
2025-05-02 10:50:03 -0300

    Update Painel_Gut_Interativo.py

--- Painel_Gut_Interativo.py
+++ Painel_Gut_Interativo.py
@@ -281,7 +281,10 @@ with aba4:
         self.set_y(-10)
         self.cell(0, 10, "Potencialize Resultados", 0, 0, "C")
 
+        
 pdf = FooterPDF()
+pdf.set_auto_page_break(auto=True, margin=15)
+pdf.alias_nb_pages()
 pdf.set_auto_page_break(auto=True, margin=15)
 pdf.alias_nb_pages()
         pdf.set_auto_page_break(auto=True, margin=15)
